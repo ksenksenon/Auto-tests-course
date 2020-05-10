51f57fcea357b21b1b6c8eb25cd106c9008498c0
Ksenia Kataeva <k.kataeva@psu.ru>
2020-05-11 00:48:27 +0500

    update task

--- Task_3_6/test_items.py
+++ Task_3_6/test_items.py
@@ -1,6 +1,6 @@
 import time
 
-def test_different_languages(browser):
+def test_find_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button = browser.find_element_by_css_selector(".btn.btn-lg.btn-primary.btn-add-to-basket")
